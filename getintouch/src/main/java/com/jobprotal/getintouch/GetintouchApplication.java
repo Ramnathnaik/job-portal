dbe5f871fedb983c18a6928070b735a34cfe7069
Ramanath K P <ramnathnaik447@gmail.com>
2024-04-20 11:52:28 +0530

    initial commit

---
+++ getintouch/src/main/java/com/jobprotal/getintouch/GetintouchApplication.java
@@ -0,0 +1,13 @@
+package com.jobprotal.getintouch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetintouchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetintouchApplication.class, args);
+	}
+
+}
---
+++ getintouch/src/test/java/com/jobprotal/getintouch/GetintouchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobprotal.getintouch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetintouchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
